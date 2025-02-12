7bda826f9e866729c3f09780ede02650438a696c
Fábio Augusto <fabio.augusto1911@gmail.com>
2025-02-12 08:20:59 -0300

    Update cv.py

--- vnd_vns/cv.py
+++ vnd_vns/cv.py
@@ -5,7 +5,8 @@ import timeit
 import random
 
 caminho_pasta = './'
-arquivos = sorted([f for f in os.listdir(caminho_pasta) if os.path.isfile(os.path.join(caminho_pasta, f))])
+arquivos = sorted([f for f in os.listdir(caminho_pasta)
+                  if os.path.isfile(os.path.join(caminho_pasta, f))])
 arquivos.remove("cv.py")
 
 def print_resultado(start, caminho, custo):
@@ -32,7 +33,8 @@ def ler_coordenadas(num_vertices, arquivo):
         nova_coord = (coor_x, coor_y)
 
         for i, coord in enumerate(coordenadas):
-            distancia = math.sqrt((nova_coord[0] - coord[0]) ** 2 + (nova_coord[1] - coord[1]) ** 2)
+            distancia = math.sqrt(
+                (nova_coord[0] - coord[0]) ** 2 + (nova_coord[1] - coord[1]) ** 2)
             matriz_montagem[len(coordenadas)][i] = distancia
             matriz_montagem[i][len(coordenadas)] = distancia
 
@@ -54,7 +56,8 @@ def grasp(matriz, alfa):
 
     while nos != []:
         # Vizinhos do nó atual: tupla (vizinho J, custo distância até J)
-        vizinhos = [(j, matriz[atual][j]) for j in nos if matriz[atual][j] != 0]
+        vizinhos = [(j, matriz[atual][j])
+                    for j in nos if matriz[atual][j] != 0]
         # Ordena os vizinhos pelo menor custo
         vizinhos = sorted(vizinhos, key=lambda x: x[1])
 
@@ -90,8 +93,10 @@ def dois_opt(custo, caminho, matriz):
         for i in range(1, len(melhor_caminho) - 2):
             for j in range(i + 1, len(melhor_caminho) - 1):
 
-                custo_removido = matriz[melhor_caminho[i-1], melhor_caminho[i]] + matriz[melhor_caminho[j], melhor_caminho[j+1]]
-                custo_adicionado = matriz[melhor_caminho[i-1], melhor_caminho[j]] + matriz[melhor_caminho[i], melhor_caminho[j+1]]
+                custo_removido = matriz[melhor_caminho[i-1], melhor_caminho[i]
+                                        ] + matriz[melhor_caminho[j], melhor_caminho[j+1]]
+                custo_adicionado = matriz[melhor_caminho[i-1], melhor_caminho[j]
+                                          ] + matriz[melhor_caminho[i], melhor_caminho[j+1]]
                 novo_custo = melhor_custo - custo_removido + custo_adicionado
 
                 if novo_custo < melhor_custo:
@@ -115,17 +120,17 @@ def swap(custo, caminho, matriz):
         for i in range(1, n - 2):
             for j in range(i + 2, n - 1):
                 custo_removido = (matriz[caminho_atual[i]][caminho_atual[i - 1]] +
-                                    matriz[caminho_atual[i]][caminho_atual[i + 1]] +
-                                    matriz[caminho_atual[j]][caminho_atual[j - 1]] +
-                                    matriz[caminho_atual[j]][caminho_atual[j + 1]])
-                
+                                  matriz[caminho_atual[i]][caminho_atual[i + 1]] +
+                                  matriz[caminho_atual[j]][caminho_atual[j - 1]] +
+                                  matriz[caminho_atual[j]][caminho_atual[j + 1]])
+
                 custo_adicionado = (matriz[caminho_atual[j]][caminho_atual[i - 1]] +
                                     matriz[caminho_atual[j]][caminho_atual[i + 1]] +
                                     matriz[caminho_atual[i]][caminho_atual[j + 1]] +
                                     matriz[caminho_atual[i]][caminho_atual[j - 1]])
-                
+
                 novo_custo = custo_atual - custo_removido + custo_adicionado
-                
+
                 if novo_custo < custo_atual:
                     caminho_atual[i], caminho_atual[j] = caminho_atual[j], caminho_atual[i]
                     custo_atual = novo_custo
@@ -135,29 +140,28 @@ def swap(custo, caminho, matriz):
                 break
         if not melhorou:
             break
-    
+
     return (caminho_atual, custo_atual)
 
 def shift(custo, caminho, matriz):
-    print("shift")
     n = len(caminho)
-    
+
     caminho_atual = caminho[:]
     custo_atual = custo
-    
+
     while True:
         melhorou = False
         for i in range(1, n - 2):
             for j in range(i + 1, n - 1):
                 custo_removido = (matriz[caminho_atual[i - 1]][caminho_atual[i]] +
-                                    matriz[caminho_atual[i]][caminho_atual[i + 1]] +
-                                    matriz[caminho_atual[j]][caminho_atual[j + 1]])
+                                  matriz[caminho_atual[i]][caminho_atual[i + 1]] +
+                                  matriz[caminho_atual[j]][caminho_atual[j + 1]])
                 custo_adicionado = (matriz[caminho_atual[i - 1]][caminho_atual[i + 1]] +
                                     matriz[caminho_atual[j]][caminho_atual[i]] +
                                     matriz[caminho_atual[i]][caminho_atual[j + 1]])
-                
+
                 novo_custo = custo_atual - custo_removido + custo_adicionado
-                
+
                 if novo_custo < custo_atual:
                     caminho_atual.insert(j, caminho_atual.pop(i))
                     custo_atual = novo_custo
@@ -167,41 +171,104 @@ def shift(custo, caminho, matriz):
                 break
         if not melhorou:
             break
-    
+
     return (caminho_atual, custo_atual)
 
 def vnd(custo, caminho, matriz):
-    estruturas = [shift, swap, dois_opt]
+    estruturas = [dois_opt, swap, shift]
     i = 0
-    
+
     while i < len(estruturas):
         novo_caminho, novo_custo = estruturas[i](custo, caminho, matriz)
-        
+
         if novo_custo < custo:
             caminho, custo = novo_caminho, novo_custo
             i = 0
         else:
             i += 1
-    
+
     return (caminho, custo)
+
+def vns(custo, caminho, matriz, vizinhanca):
+    i = random.randint(1, len(matriz[0]) - 2)
+    j = random.randint(i + 1, len(matriz[0]) - 1)
+
+    # dois_opt
+    if (vizinhanca == 0):
+        custo_removido = matriz[caminho[i-1], caminho[i]
+                                ] + matriz[caminho[j], caminho[j+1]]
+        custo_adicionado = matriz[caminho[i-1],
+                                  caminho[j]] + matriz[caminho[i], caminho[j+1]]
+        novo_custo = custo - custo_removido + custo_adicionado
+
+        novo_caminho = caminho[:i] + caminho[i:j+1][::-1] + caminho[j+1:]
+
+        return (novo_caminho, novo_custo)
+    # swap
+    elif vizinhanca == 1:
+        custo_removido = (matriz[caminho[i]][caminho[i - 1]] +
+                          matriz[caminho[i]][caminho[i + 1]] +
+                          matriz[caminho[j]][caminho[j - 1]] +
+                          matriz[caminho[j]][caminho[j + 1]])
+        custo_adicionado = (matriz[caminho[j]][caminho[i - 1]] +
+                            matriz[caminho[j]][caminho[i + 1]] +
+                            matriz[caminho[i]][caminho[j + 1]] +
+                            matriz[caminho[i]][caminho[j - 1]])
+        novo_custo = custo - custo_removido + custo_adicionado
+
+        novo_caminho = caminho.copy()
+        novo_caminho[i], novo_caminho[j] = novo_caminho[j], novo_caminho[i]
+
+        return (novo_caminho, novo_custo)
+    # shift
+    else:
+        custo_removido = (matriz[caminho[i - 1]][caminho[i]] +
+                          matriz[caminho[i]][caminho[i + 1]] +
+                          matriz[caminho[j]][caminho[j + 1]])
+        custo_adicionado = (matriz[caminho[i - 1]][caminho[i + 1]] +
+                            matriz[caminho[j]][caminho[i]] +
+                            matriz[caminho[i]][caminho[j + 1]])
+        novo_custo = custo - custo_removido + custo_adicionado
+
+        novo_caminho = caminho.copy()
+        novo_caminho.insert(j, novo_caminho.pop(i))
+
+        return (caminho, custo)
 
 def grasp_vnd(matriz):
     start = timeit.default_timer()
     melhor_caminho = None
     melhor_custo = float('inf')
-    
-    for i in range(11):
-        alpha = i / 10
-        caminho_grasp, custo_grasp = grasp(matriz, alpha)
-        caminho_otimizado, custo_otimizado = vnd(custo_grasp, caminho_grasp, matriz)
-        
-        if custo_otimizado < melhor_custo:
-            melhor_caminho, melhor_custo = caminho_otimizado, custo_otimizado
-    
-    print("Melhor solução encontrada com GRASP + VND:")
+
+    alpha = 0.3
+    caminho_grasp, custo_grasp = grasp(matriz, alpha)
+    caminho_otimizado, custo_otimizado = vnd(
+        custo_grasp, caminho_grasp, matriz)
+
+    if custo_otimizado < melhor_custo:
+        melhor_caminho, melhor_custo = caminho_otimizado, custo_otimizado
+
     print_resultado(start, melhor_caminho, melhor_custo)
-    
+
     return (melhor_caminho, melhor_custo)
+
+def grasp_vns_nds(matriz):
+    start = timeit.default_timer()
+    caminho, custo = grasp(matriz, 0.3)
+    vizinhanca = 0
+
+    while vizinhanca < 3:
+        novo_caminho, novo_custo = vns(custo, caminho, matriz, vizinhanca)
+        caminho_otimizado, custo_otimizado = vnd(novo_custo, novo_caminho, matriz)
+
+        if custo_otimizado < custo:
+            caminho, custo = caminho_otimizado, custo_otimizado
+            vizinhanca = 0
+        else:
+            vizinhanca += 1
+    
+    print_resultado(start, caminho, custo)
+    return (caminho, custo)
 
 for arquivo in arquivos:
     matriz_distancias = None
@@ -217,4 +284,7 @@ for arquivo in arquivos:
         matriz_distancias = ler_coordenadas(num_vertices, file)
 
         print(f"Arquivo: {arquivo} ----------------------------------")
+        print("VND")
         (custo, caminho) = grasp_vnd(matriz_distancias)
+        print("VNS")
+        (custo, caminho) = grasp_vns_nds(matriz_distancias)
